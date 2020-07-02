40527343a195a57c4d1d6ca3f615714e34a8f7c5
Sai Kiran <Sai>
2020-07-02 09:17:28 +0530

    Order Service Management System

---
+++ orderservice-management/src/main/java/com/order/service/management/OrderserviceManagementApplication.java
@@ -0,0 +1,15 @@
+package com.order.service.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.order.service.management.feignclient")
+public class OrderserviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceManagementApplication.class, args);
+	}
+
+}
