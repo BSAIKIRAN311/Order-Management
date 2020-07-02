b254ee861c205dc3d1da5f960cf099024fa89cba
Sai Kiran <Sai>
2020-07-02 09:24:46 +0530

    Order Service Management System

---
+++ orderItem-management/src/main/java/com/order/item/management/OrderItemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.order.item.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderItemManagementApplication.class, args);
+	}
+
+}
